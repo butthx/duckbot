1a16584f2aa64cdb076cdf5b73565ec87355c336
Glitch (jungle-viridian-fairy) <none>
2021-02-10 07:39:12 +0000

    👓🖖 Checkpoint ./bot/eval.js:25627213/1596

--- bot/eval.js
+++ bot/eval.js
@@ -1,25 +1,30 @@
-const {Markup} = require('telegraf');
-const util = require('./util')
+const { Markup } = require("telegraf");
+const util = require("./util");
 const eval_module = {
-  telegraf : async function (ctx){
+  telegraf: async function(ctx) {
     try {
-      if(ctx.message.chat.id !== -1001492226458){
-        return util.kirimpesan(ctx,'Chat owner to get access to this feature!')
+      if (ctx.message.chat.id !== -1001492226458) {
+        return util.kirimpesan(
+          ctx,
+          "Chat owner to get access to this feature!"
+        );
       }
-    let func = ctx.message.text.split(' ')
-    let def = ctx.message.text.replace(func[0],'').trim()
-    if(def == ''){
-      util.kirimpesan(ctx,'/tele <telegraf function>')
-    }else{
-    if(/console\.log/gmi.exec(def)){
-      def = def.replace(/console\.log/gmi,'ctx.reply')
-    }
-    return eval(`(async () => {try{${def}}catch(e){ctx.reply(e.message)}})()`)
-    }
+      let func = ctx.message.text.split(" ");
+      let def = ctx.message.text.replace(func[0], "").trim();
+      if (def == "") {
+        util.kirimpesan(ctx, "/tele &lt;telegraf function&gt;");
+      } else {
+        if (/console\.log/gim.exec(def)) {
+          def = def.replace(/console\.log/gim, "ctx.reply");
+        }
+        return eval(
+          `(async () => {try{${def}}catch(e){ctx.reply(e.message)}})()`
+        );
+      }
     } catch (e) {
-      return ctx.reply(e.message)
+      return ctx.reply(e.message);
     }
   }
-}
+};
 
-module.exports = eval_module
+module.exports = eval_module;
