34f6245fd43a25cf67fe924224d0c118265358f2
Glitch (jungle-viridian-fairy) <none>
2021-03-04 06:51:09 +0000

    👨‍❤️‍👨💏 Checkpoint ./bot/database/groups.js:25627213/6144

--- bot/database/groups.js
+++ bot/database/groups.js
@@ -1,45 +1,114 @@
-const mongoose = require ('mongoose')
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const groupsData = new Schema({
-  chat_id : {
-    type : Number
+  chat_id: {
+    type: Number
   },
   language: {
-   type : String
+    type: String
   },
-  rules : {
-    type : String
+  rules: {
+    type: String
   },
-  blacklist : {
-    triger : {
-      type : Array
+  blacklist: {
+    triger: {
+      type: Array
     },
-    action : {
-      type : String
-   }
+    action: {
+      type: String
+    }
   },
- welcome : {
-   status : {
-     type : Boolean
-   },
-   captcha : {
-     status : {
-       type : Boolean
-     },
-     captchaType : {
-       type : String
-     },
-     time : {
-       type : String
-     }
-   },
-   deleteOldMessage : {
-    status : {
-      type : Boolean
+  welcome: {
+    status: {
+      type: Boolean
     },
-    message_id : {
-      type : 
+    captcha: {
+      status: {
+        type: Boolean
+      },
+      captchaType: {
+        type: String
+      },
+      time: {
+        type: String
+      }
+    },
+    deleteOldMessage: {
+      status: {
+        type: Boolean
+      },
+      message_id: {
+        type: Number
+      }
+    },
+    wlcType: {
+      type: String
+    },
+    value: {
+      type: String
+    }
+  },
+  goodbye: {
+    status: {
+      type: Boolean
+    },
+    deleteOldMessage: {
+      status: {
+        type: Boolean
+      },
+      message_id: {
+        type: Number
+      }
+    },
+    gbType: {
+      type: String
+    },
+    value: {
+      type: String
+    }
+  },
+  notes: {
+    value: {
+      type: Array
+    },
+    deleteOldMessage: {
+      status: {
+        type: Boolean
+      },
+      message_id: {
+        type: Number
+      }
+    }
+  },
+  filters: {
+    value: {
+      type: Array
+    },
+    deleteOldMessage: {
+      status: {
+        type: Boolean
+      },
+      message_id: {
+        type: Number
+      }
+    }
+  },
+  admins: {
+    type: Array
+  },
+  users: {
+    type: Array
+  },
+  settings: {
+    sangmata: {
+      type: Boolean
+    },
+    cas: {
+      type: Boolean
+    },
+    spamWatch: {
+      type: Boolean
     }
   }
- }
-})
+});
+module.exports = mongoose.model('groups',groupsData,'chat')
