2612f7ed8d15596e5c583956b54dc62b2a09d58e
Glitch (jungle-viridian-fairy) <none>
2021-03-21 09:36:03 +0000

    🎻🏏 Checkpoint ./index.ts:25627213/549

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("hai")
+import dotenv from 'dotenv'
+dotenv.config() 
+import {Telegraf} from 'telegraf'
+const bot = new Telegraf(pr
+bot.command('start',(ctx)=>{
+  console.log(ctx)
+})
+bot.launch().then(()=>{
+  console.log('Running..')
+})
