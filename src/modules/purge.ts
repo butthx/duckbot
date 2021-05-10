ee4c82c1aaa6551255caf52fabe59c10710c179f
butthx <tanya.duwi23@gmail.com>
2021-05-10 10:03:20 +0700

    502 - canary

--- src/modules/purge.ts
+++ src/modules/purge.ts
@@ -29,9 +29,7 @@ export async function purge(ctx) {
         chat_id: ctx.chat.id,
         message_id: yesterday++
       }, (now, list)=> {
-        ctx.telegram.deleteMessage(now.chat_id, now.message_id).catch(e) {
-          return e
-        }
+        ctx.telegram.deleteMessage(now.chat_id, now.message_id).catch((e)=>return e)
       })
     }
   }catch(error) {
